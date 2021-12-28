7ee82a2df37468a6a7a77ac7b33bfc3deb93bfbe
alvfaye <alvfaye@gmail.com>
2021-12-28 14:35:21 +0800

    added env file

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
+let port = process.env.PORT;
+let uri = process.env.MONGO_URI;
 
 const app = express();
 app.get('/', (req, res) => {
   res.send('Kamusta pilipinas?');
 });
 
-mongoose.connect(
-  'mongodb+srv://mafea8ph:administrator@cluster0.sooma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
@@ -20,6 +20,6 @@ db.once('open', function () {
   console.log('Connected successfully');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is running at port 3000');
 });
